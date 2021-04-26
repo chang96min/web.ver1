d960f897fcd4ded015e0f9fb127e5928ca6eb618
chang96min <ckdals8785@naver.com>
2021-04-26 11:26:57 +0900

    first commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!-- �ϴ� ���� -->
+<div class="footer">
+
+	<div>
+		<h1>Light</h1>
+		<p>대구광역시 동내로 70</p>
+		<p>대표자명 : 이창민</p>
+		<p>이메일 : ckdals8785@naver.com</p>
+		<p>전화번호 : 010-1234-5678</p>
+		<span>copyright(c) Light. All rights reserved.</span>
+	</div>
+
+</div>
+
+
+
+
+<script>
+  AOS.init();
+</script>
